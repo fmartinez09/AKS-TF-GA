32fa11c341dbb45e0226da9b145868098c8c6755
Fernando Martinez <rfernandomartinez@gmail.com>
2024-11-15 08:35:20 -0300

    change tf node pool

--- aks/main.tf
+++ aks/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_kubernetes_cluster" "k8squickstart" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_B1ms"
+    vm_size    = "Standard_D2_v3"
     
   }
 
